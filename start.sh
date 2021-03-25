59fe4f31f44ab91213cef70b9799469a82b0d4a7
QuercusNick <nickknowing@gmail.com>
2021-03-25 18:49:30 +0200

    Cleanup old containers
    Signed-off-by: QuercusNick <nickknowing@gmail.com>

--- start.sh
+++ start.sh
@@ -2,6 +2,11 @@
 
 docker build -t testtask:latest -f Docerfile.dockerfile .
 
+docker rm -f ansible_host
+docker rm -f target1
+docker rm -f target2
+docker rm -f target3
+
 docker run --privileged -d -p 2222:22 --name ansible_host testtask:latest
 docker run --privileged -d --name target1 testtask:latest
 docker run --privileged -d --name target2 testtask:latest
